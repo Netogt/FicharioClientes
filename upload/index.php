c8e27cd3c6b3de2047dcf9a04fdabc415c62e76a
Luiz Neto <98713773+Netogt@users.noreply.github.com>
2022-05-16 09:47:27 -0300

    uploadImagens

---
+++ upload/index.php
@@ -0,0 +1,10 @@
+<form action="upload0.php" method="post" enctype='multipart/form-data' >
+
+<div class="mb-3">
+  <label for="formFile" class="form-label">Default file input example</label>
+  <input class="form-control" type="file" id="formFile" name="dropzone">
+</div>
+
+<button type="submit">Upload</button>
+
+</form>
---
+++ upload/upload.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+$uploaddir = '/uploads/';
+
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+
+
+
+    echo "Arquivo válido e enviado com sucesso.\n";
+
+
+
+} else {
+
+
+
+    echo "Possível ataque de upload de arquivo!\n";
+
+
+}
+
+
+
+?>
